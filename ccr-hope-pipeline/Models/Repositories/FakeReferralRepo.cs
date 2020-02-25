84a1c0b47cdb1cb4f9f177ca1f720d600fd4c0a2
chroscickid <n00963091@unf.edu>
2020-02-25 11:00:27 -0500

    Manually merging Brandi's file but not like a troglodyte

---
+++ ccr-hope-pipeline/Models/Repositories/FakeReferralRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HopePipeline.Models
+{
+    public class FakeReferralRepo : ReferralRepository
+    {
+        public IEnumerable<Referral> Referrals => new List<Referral>
+        {
+            new Referral { pK = 0, fName = "David", lName = "Chroscicki" , dOB = new DateTime(1996, 10, 17), status = "Pending"},
+            new Referral { pK = 1, fName = "Bridget", lName = "Lyon" , dOB = new DateTime(2002, 2, 10), status = "Pending"},
+            new Referral { pK = 2, fName = "Kristina", lName = "Huston" , dOB = new DateTime(1998, 4, 13), status = "Pending"},
+            new Referral { pK = 3, fName = "Lili", lName = "Weinstein" , dOB = new DateTime(1998, 4, 15), status = "Pending"}
+
+        };
+    }
+}

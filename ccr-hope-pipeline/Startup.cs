f9609726062a2812d7f44585a23bfe6d36a93a2a
chroscickid <n00963091@unf.edu>
2020-04-28 17:58:10 -0400

    Hellp!

--- ccr-hope-pipeline/Startup.cs
+++ ccr-hope-pipeline/Startup.cs
@@ -14,7 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using dotenv.net;
 
-namespace ccr_hope_pipeline
+namespace HopePipeline
 {
     public class Startup
     {
